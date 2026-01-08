706bbc2239bb5aabc4b08de560d2126dc946a478
Abdelziz Khayroune <khayrouneabdelaziz3@gmail.com>
2026-01-08 21:21:56 +0100

    Add project files except target

---
+++ bonjour-devops/src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("bonjour et bon courage  dans votre projet en devops");
+    }
+}
